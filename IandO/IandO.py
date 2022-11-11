f75edf1413c5664566edc7403c5521c1e6a24863
wario420 <christian-jamal.larsen@stud.uni-bamberg.de>
2022-11-11 14:29:39 +0100

    Import n specified number of rows with optional parameter nrows.

--- IandO/IandO.py
+++ IandO/IandO.py
@@ -32,37 +32,42 @@ def __find_path_column_list_in_config(config_file_path: str, file_identifier: st
     return 0
 
 
-def handle_value_error_for_single_csv_import(config_tuple: tuple, list_of_headers_from_configfile: object) -> object:
+def handle_value_error_for_single_csv_import(config_tuple: tuple, list_of_headers_from_configfile: object, nrows=1) -> object:
     """If no columns are specified in config file return all columns in dataframe - avoid value error."""
     try:
-        return pandas.read_csv(config_tuple[0], sep=None, header=0, usecols=list_of_headers_from_configfile,
-                               engine='python', encoding_errors='replace', on_bad_lines="skip")
+        if nrows == 1:
+            return pandas.read_csv(config_tuple[0], sep=None, header=0, usecols=list_of_headers_from_configfile,
+                                   engine='python', encoding_errors='replace', on_bad_lines="skip")
+        else:
+            return pandas.read_csv(config_tuple[0], sep=None, header=0, usecols=list_of_headers_from_configfile,
+                                   engine='python', encoding_errors='replace', on_bad_lines="skip", nrows=nrows)
     except ValueError:
         return pandas.read_csv(config_tuple[0], sep=None, header=0, engine='python', encoding_errors='replace',
                                on_bad_lines="skip")
 
 
-def import_single_file_as_UTF(config_file_path: str, file_identifier: str):
+def import_single_file_as_UTF(config_file_path: str, file_identifier: str, nrows=1):
     """Import a single csv file to pandas dataframe with error handling.
     Shouldn't be used by itself."""
     config_tuple = (__find_path_column_list_in_config(config_file_path, file_identifier))
     # TODO: handle if config columns have been written with ',' and not with ';'
     list_of_headers_from_configfile = config_tuple[1].split(sep=';')
-    return handle_value_error_for_single_csv_import(config_tuple, list_of_headers_from_configfile)
+    return handle_value_error_for_single_csv_import(config_tuple, list_of_headers_from_configfile, nrows)
 
 
-def import_all_config_specified_csv(file_list: str, config_file_path: str) -> dict:
+def import_all_config_specified_csv(file_list: str, config_file_path: str, nrows=1) -> dict:
     """Main function to be called in this module importing all in the fileconfig.txt specified csv files to a dict.
     \nInput:
     (1)list[] of file identifiers as specified in fileconfig. Column headers need to be specified!
     Otherwise error will be thrown.
     \n(2) File_path to config file.
+    (3) Number of rows to be imported 1 = import all rows != 1 import n rows
     \nOutput:
     \n(1) Dictionary containing all imported csv as key = file identifier, value = dataframe from csv"""
     dataframe_dictionary_of_imported_csv = {}
     try:
         for x in file_list:
-            imported_csv = import_single_file_as_UTF(config_file_path, x)
+            imported_csv = import_single_file_as_UTF(config_file_path, x, nrows)
             dataframe_dictionary_of_imported_csv.__setitem__(x, imported_csv)
     except TypeError:
         print(f'Error: The file "{x}" you tried to import does not exist, '
