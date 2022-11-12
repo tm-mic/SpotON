53f5f86c1d2e8832aaaa5eaae5e66b819152644f
wario420 <christian-jamal.larsen@stud.uni-bamberg.de>
2022-11-12 16:37:18 +0100

    feature: pretty printing dataframes in the terminal

---
+++ pd_display_options.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+pd.set_option('display.colheader_justify', 'center')
+pd.set_option('display.precision', 2)
+
