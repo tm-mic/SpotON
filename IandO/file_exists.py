a14da8cfa8293354fd019f768171a401db3db165
wario420 <christian-jamal.larsen@stud.uni-bamberg.de>
2023-02-01 11:19:55 +0100

    support file checking in os through config

--- IandO/file_exists.py
+++ IandO/file_exists.py
@@ -1,13 +1,45 @@
 import os
-from IandO import json_utility as ju
 
 
 def path_exists(path):
+    """
+    Check if a path exists in the directory.
+
+    :param path: Path in directory.
+    :return: Bool if path exists.
+    """
     return os.path.exists(path)
 
 
 def setup_folders(path):
+    """
+    Creates a folder at the given path. If folder already exists pass.
+
+    :param path:
+    :return: None
+    """
     try:
         os.makedirs(path, exist_ok=True)
     except FileExistsError:
         pass
+    return
+
+
+def files_exists(path):
+    """
+    Check if a file exists at a given path. If not asks the user to add the file at the provided path.
+    Re-runs until a file is provided at the given path.
+
+    :param path: Path to file.
+    :return: None
+    """
+    c = 0
+    while c == 0:
+        if path_exists(path):
+            c = 1
+        else:
+            c = 0
+            print(f"The corresponding file to the given filepath {path} does not exist. Please provide the correct"
+                  f" file at the path given in the config.json.")
+            input("After you added the file enter any key to continue.")
+    return
